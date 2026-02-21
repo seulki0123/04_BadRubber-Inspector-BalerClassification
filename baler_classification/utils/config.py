fee141177e56120aaca821e8ad655f886099de51
seulki0123 <seulki@infrax.co.kr>
2026-02-21 18:08:58 +0900

    fix: config 로드 시 config.yaml의 baler_classification 섹션만 사용

--- baler_classification/utils/config.py
+++ baler_classification/utils/config.py
@@ -2,5 +2,5 @@ import yaml
 
 def load_config():
     with open("config.yaml", "r") as f:
-        config = yaml.safe_load(f)
+        config = yaml.safe_load(f)["baler_classification"]
     return config
