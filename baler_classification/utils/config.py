c0f1e534b86266401497585d4764be821947a948
seulki0123 <seulki@infrax.co.kr>
2026-03-27 02:13:48 +0900

    load_config encoding 추가

--- baler_classification/utils/config.py
+++ baler_classification/utils/config.py
@@ -1,6 +1,6 @@
 import yaml
 
 def load_config():
-    with open("config.yaml", "r") as f:
+    with open("config.yaml", "r", encoding="utf-8") as f:
         config = yaml.safe_load(f)["baler_classification"]
     return config
